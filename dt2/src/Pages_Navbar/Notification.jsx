ab1b8111aa2759b9bf4f4df711f98555d8dba8f2
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-07 19:33:20 +0530

    Delete dt2 directory

--- dt2/src/Pages_Navbar/Notification.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export default function () {
-  return ( 
-  <h1>Notification</h1>
-  );
-}
--- dt2/src/Pages_Navbar/Setting.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export default function () {
-  return ( 
-  <h1>Settings</h1>
-  );
-}
