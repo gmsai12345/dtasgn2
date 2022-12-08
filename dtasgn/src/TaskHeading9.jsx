9876a622bcff45a5ca2640210c2f812fe65d5620
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-08 08:52:03 +0530

    Update TaskHeading9.jsx

--- dtasgn/src/TaskHeading9.jsx
+++ dtasgn/src/TaskHeading9.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 export default function () {
+  const navigate = useNavigate();
+
+  const navigateTo8 = () => {
+    navigate("/TaskHeading8");
+  };
   return (
     <div>
       <h1>Analytics page</h1>
-      <img src="https://img.icons8.com/ios-filled/50/000000/back.png" alt = ""/>
+      <span onClick={navigateTo8}>
+        <img
+          src="https://img.icons8.com/ios-filled/50/000000/back.png"
+          alt=""
+        />
+      </span>
     </div>
   );
 }
