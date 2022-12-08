c2b1978689e45eedf6788a65122c5f556879af65
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-08 13:44:06 +0530

    Update TaskHeading9.jsx

--- dtasgn/src/TaskHeading9.jsx
+++ dtasgn/src/TaskHeading9.jsx
@@ -1,20 +1,32 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-export default function () {
-  const navigate = useNavigate();
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+//import { useNavigate } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
-  const navigateTo8 = () => {
-    navigate("/TaskHeading8");
-  };
+export default function () {
+  function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+    return (
+      <li className={isActive ? "active" : ""}>
+        <Link to={to} {...props}>
+          {children}
+        </Link>
+      </li>
+    );
+  }
   return (
     <div>
       <h1>Analytics page</h1>
-      <span onClick={navigateTo8}>
+      <figure align="right">
+        <KeyboardArrowDownIcon />
+      </figure>
+      <CustomLink to="/TaskHeading9">
         <img
           src="https://img.icons8.com/ios-filled/50/000000/back.png"
           alt=""
         />
-      </span>
+      </CustomLink>
     </div>
   );
 }
