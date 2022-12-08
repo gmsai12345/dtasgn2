ca94e3641dbfef62c5aa71f74525dd1bf47d488c
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-08 08:49:19 +0530

    Update TaskHeading3.jsx

--- dtasgn/src/TaskHeading3.jsx
+++ dtasgn/src/TaskHeading3.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { useNavigate } from "react-router-dom";
+import TaskHeading4 from "./TaskHeading4";
+import TaskHeading2 from "./TaskHeading2";
 export default function () {
+  const navigate = useNavigate();
+
+  const navigateTo4 = () => {
+    navigate("/TaskHeading4");
+  };
+  const navigateTo2 = () => {
+    navigate("/TaskHeading2");
+  };
   return (
     <div>
       <div className="TaskHeading3">
@@ -13,9 +24,18 @@ export default function () {
           <KeyboardArrowDownIcon />
         </div>
       </div>
-      <img src="https://img.icons8.com/ios/50/000000/forward--v1.png" alt = ""/>
-
-      <img src="https://img.icons8.com/ios-filled/50/000000/back.png" alt = ""/>
+      <div onclick={navigateTo4}>
+        <img
+          src="https://img.icons8.com/ios/50/000000/forward--v1.png"
+          alt=""
+        />
+      </div>
+      <span onClick={navigateTo2}>
+        <img
+          src="https://img.icons8.com/ios-filled/50/000000/back.png"
+          alt=""
+        />
+      </span>
     </div>
   );
 }
