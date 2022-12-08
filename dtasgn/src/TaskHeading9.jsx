4322924241f585afb070fd4aa08c6e0c1b2065ff
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-08 07:52:55 +0530

    Update TaskHeading9.jsx

--- dtasgn/src/TaskHeading9.jsx
+++ dtasgn/src/TaskHeading9.jsx
@@ -3,6 +3,7 @@ export default function () {
   return (
     <div>
       <h1>Analytics page</h1>
+      <img src="https://img.icons8.com/ios-filled/50/000000/back.png" alt = ""/>
     </div>
   );
 }
