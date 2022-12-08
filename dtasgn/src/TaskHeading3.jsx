e7539ebd8dbdb0a3f495b18338770233cb29f4be
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-08 13:21:43 +0530

    Update TaskHeading3.jsx

--- dtasgn/src/TaskHeading3.jsx
+++ dtasgn/src/TaskHeading3.jsx
@@ -3,15 +3,8 @@ import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { useNavigate } from "react-router-dom";
 import TaskHeading4 from "./TaskHeading4";
 import TaskHeading2 from "./TaskHeading2";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 export default function () {
-  const navigate = useNavigate();
-
-  const navigateTo4 = () => {
-    navigate("/TaskHeading4");
-  };
-  const navigateTo2 = () => {
-    navigate("/TaskHeading2");
-  };
   return (
     <div>
       <div className="TaskHeading3">
@@ -21,21 +14,35 @@ export default function () {
             <input type="text" id="ip1" />
           </div>
           <br />
-          <KeyboardArrowDownIcon />
         </div>
+        <figure align="right">
+          <KeyboardArrowDownIcon />
+        </figure>
       </div>
-      <div onclick={navigateTo4}>
+      <CustomLink to="/TaskHeading4">
         <img
           src="https://img.icons8.com/ios/50/000000/forward--v1.png"
           alt=""
         />
-      </div>
-      <span onClick={navigateTo2}>
+      </CustomLink>
+      <CustomLink to="/TaskHeading2">
         <img
           src="https://img.icons8.com/ios-filled/50/000000/back.png"
           alt=""
         />
-      </span>
+      </CustomLink>
     </div>
+  );
+}
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
   );
 }
