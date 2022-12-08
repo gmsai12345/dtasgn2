875b9c0289a2ca9d863462b243b33dc4b086b781
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-08 19:56:35 +0530

    Update TaskHeading4.jsx

--- dt3/src/TaskHeading4.jsx
+++ dt3/src/TaskHeading4.jsx
@@ -8,10 +8,14 @@ export default function () {
   return (
     <div>
       <div className="TaskHeading4">
+      <h1 className = "title">Task Heading 4</h1>
         <div className="Task4">
-          <h1>Task Heading 4</h1>
+          
 
-          <embed src="https://webhome.phy.duke.edu/~rgb/Class/intro_physics_1/intro_physics_1.pdf" />
+          <embed
+            className="embed"
+            src="https://webhome.phy.duke.edu/~rgb/Class/intro_physics_1/intro_physics_1.pdf"
+          />
           <br />
         </div>
         <figure align="right">
